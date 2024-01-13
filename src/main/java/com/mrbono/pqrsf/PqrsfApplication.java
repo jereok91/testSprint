05d17d970e6168026cef3284a2f19b39e0e184b7
jereok91 <jereok91@gmail.com>
2024-01-13 08:51:03 -0500

    feature: add rest file

---
+++ src/main/java/com/mrbono/pqrsf/PqrsfApplication.java
@@ -0,0 +1,13 @@
+package com.mrbono.pqrsf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PqrsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PqrsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrbono/pqrsf/PqrsfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrbono.pqrsf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PqrsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
